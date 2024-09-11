2209cf19718d61b08b97257323ddea13a9166746
Britnay Gore <britnaygore268@gmail.com>
2024-09-10 21:33:26 -0400

    Updated DbContext to seed data to database

--- RareGroup_BEDbContext.cs
+++ RareGroup_BEDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RareGroup.Data;
+using RareGroup_BE.Data;
 using RareGroup_BE.Models;
 namespace RareGroup_BE;
 
 public class RareGroup_BEDbContext : DbContext
 {
-    public DbSet<Categories> Categories { get; set; }
-    public DbSet<Posts> Posts { get; set; }
-    public DbSet<Tags> Tags { get; set; }
-    public DbSet<Users> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public RareGroup_BEDbContext(DbContextOptions<RareGroup_BEDbContext> context) : base(context)
     {
@@ -16,6 +18,16 @@ public class RareGroup_BEDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Send data to Categories table
+        modelBuilder.Entity<Category>().HasData(CategoryData.Categories);
 
+        // Sned data to Posts table
+        modelBuilder.Entity<Post>().HasData(PostData.Posts);
+
+        // Send data to Tags table
+        modelBuilder.Entity<Tag>().HasData(TagData.Tags);
+
+        // Send data to Users table
+        modelBuilder.Entity<User>().HasData(UserData.Users);
     }
 }
