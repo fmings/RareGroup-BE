521b56de7547c3ed0e6459d8da2ca1e13157674a
Britnay Gore <britnaygore268@gmail.com>
2024-09-10 20:16:21 -0400

    update Program.cs and created DbContext file and merged new main

---
+++ RareGroup_BEDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace RareGroup_BE;
+
+public class RareGroup_BEDbContext : DbContext
+{
+    public RareGroup_BEDbContext(DbContextOptions<RareGroup_BEDbContext> context) : base(context)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
