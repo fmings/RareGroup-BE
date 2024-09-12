ba3d464fcd16f0a6e945269ca2dbe149eb472c89
Britnay Gore <britnaygore268@gmail.com>
2024-09-11 20:50:06 -0400

    Removed get user posts by API

--- API/PostAPI.cs
+++ API/PostAPI.cs
@@ -5,29 +5,6 @@ namespace RareGroup_BE.API
 	{
         public static void Map(WebApplication app)
         {
-            // Get User's Posts
-			app.MapGet("/api/user/{userId}/posts", (RareGroup_BEDbContext db, int userId) =>
-			{
-                if (userId == null)
-                {
-                    return Results.NotFound("The userId does not exist");
-                }
-
-                return Results.Ok(db.Users.Select(user => new
-                {
-                    Id = user.Id,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Email = user.Email,
-                    Bio = user.Bio,
-                    CreatedOn = user.CreatedOn,
-                    IsStaff = user.IsStaff,
-                    Uid = user.Uid,
-                    Active = user.Active,
-                    Posts = user.Posts
-
-                }).SingleOrDefault(user => user.Id == userId));
-            });
 		}
 	}
 }
