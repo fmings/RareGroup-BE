9629f9ca68d8898a61131b58b95d1fc3eeff5487
zach colburn <colburn.zach7@gmail.com>
2024-09-12 21:21:46 -0500

    testing PR

--- API/PostAPI.cs
+++ API/PostAPI.cs
@@ -7,12 +7,13 @@ namespace RareGroup_BE.API
 	{
         public static void Map(WebApplication app)
         {
-			//GET ALL POSTS
+			// GET all Posts
 			app.MapGet("/api/posts", (RareGroup_BEDbContext db) =>
 			{
 				return db.Posts.ToList();
 			});
 
+            // GET Post by ID
 			app.MapGet("/api/post/{postId}", async (RareGroup_BEDbContext db, int id) =>
 			{
                 Post post = await db.Posts
@@ -28,7 +29,52 @@ namespace RareGroup_BE.API
                 return Results.Ok(post);
 
             });
-		}
+
+            // POST new Post
+            app.MapPost("/api/post", (RareGroup_BEDbContext db, Post post) =>
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return Results.Created($"/api/post/{post.Id}", post);
+            });
+
+            // DELETE Post
+            app.MapDelete("/posts/{id}", (RareGroup_BEDbContext db, int id) =>
+            {
+                Post post = db.Posts.FirstOrDefault(p => p.Id == id);
+
+                if (post == null)
+                {
+                    return Results.NotFound();
+                }
+
+                db.Posts.Remove(post);
+                db.SaveChanges();
+
+                return Results.NoContent();
+            });
+
+            // Edit Post
+            app.MapPut("/post/{postId}", (RareGroup_BEDbContext db, int id, Post updatedPost) =>
+            {
+                Post postToUpdate = db.Posts.SingleOrDefault(p => p.Id == id);
+
+                if (postToUpdate == null)
+                {
+                    return Results.NotFound();
+                }
+                postToUpdate.Title = updatedPost.Title;
+                postToUpdate.PublicationDate = updatedPost.PublicationDate;
+                postToUpdate.ImageUrl = updatedPost.ImageUrl;
+                postToUpdate.Content = updatedPost.Content;
+                postToUpdate.Approved = updatedPost.Approved;
+                postToUpdate.UserId = updatedPost.UserId;
+                postToUpdate.CategoryId = updatedPost.CategoryId;
+
+                db.SaveChanges();
+                return Results.Ok(postToUpdate);
+            });
+        }
 	}
 }
 
